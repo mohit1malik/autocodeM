cca316612bc388c3d444c0e0f7d4a38b3f72d908
mohitJunglee <mohit@jungleegames.com>
2019-05-08 12:38:40 +0530

    Spring Project Code First commit

---
+++ HelloSpring/src/main/java/HelloSpring/HelloSpring/App.java
@@ -0,0 +1,13 @@
+package HelloSpring.HelloSpring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
