f01a54417e3388f6bfb5ae480fd4e3495b891ac5
mohit1malik <mohit1malik@gmail.com>
2021-02-09 15:06:23 +0530

    Airthmetic Operators Examples

--- learn/src/test/java/me/learning/learn/AirthmeticOperators.java
+++ learn/src/test/java/me/learning/learn/AirthmeticOperators.java
@@ -9,12 +9,12 @@ public static void main(String[] args) {
     int a =4;
     int b = 5;
 
-    b +=a;
-    int c = a++;
-    int d = ++a;
-    int e = --a;
-    int f = a--;
-    int g = a++ -b;
+    b +=a; //b = b+a , b 5+4 =  9, b=9, a=4
+    int c = a++; // c = a  , a = a+1, c=4, a =5 
+    int d = ++a; // a = a+1; d = a, d = 6 a =6;
+    int e = --a; // a = a-1 , e =a , e = 5 , a = 5
+    int f = a--; // f = a , a = a-1 ,  f =5 , a = 4 
+    int g = a++ -b; // a = 4 , g = a - b , g = -5, a = 5  
 
     System.out.println("value of a " +a);
     System.out.println("value of b " +b);
@@ -23,6 +23,8 @@ public static void main(String[] args) {
     System.out.println("value of e " +e);
     System.out.println("value of f " +f);
     System.out.println("value of g " +g);
+
+
 
 
 
